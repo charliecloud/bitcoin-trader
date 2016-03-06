10c09ea9432d05dc1ac0a04797edca4e60f888bc
charliecloud <meredithevon@Merediths-MacBook-Pro.local>
2016-03-06 17:38:44 -0500

    Adding EmailTransaction class
    Class that will be used to interact with emails.

---
+++ lib/email_transaction.rb
@@ -0,0 +1,34 @@
+require 'gmail'
+
+class EmailTransaction
+  @gmail = nil
+ 
+  def initialize(email,pw)
+    @gmail = Gmail.new(email, pw)
+  end
+  
+  def send_email(fromAdd, toAdd, eSub, eBody)
+    #TODO: Refactor to use symbols
+    return false unless !@gmail.nil?
+    email = @gmail.generate_message do
+      from fromAdd
+      to toAdd
+      subject eSub
+      body eBody
+    end
+    email.deliver! 
+  end
+  
+  def get_emails(peek=false, fromAdd)
+    return false unless !@gmail.nil?
+    #Mark it as read?
+    @gmail.peek = peek
+    messages = @gmail.inbox.emails(:unread, :from => fromAdd)
+    messages
+  end
+  
+  def close_email
+    @gmail.logout unless @gmail.nil?
+  end
+    
+end
