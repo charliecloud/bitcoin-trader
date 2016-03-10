57482063b166503842d8592d9b7cbfb0f26b20d2
charliecloud <meredithevon@Merediths-MacBook-Pro.local>
2016-03-10 14:44:45 -0500

    Added File Logging
    Implemented file logging for btw transactions within the class

--- lib/coinbase_transaction.rb
+++ lib/coinbase_transaction.rb
@@ -5,10 +5,14 @@ class CoinbaseTransaction
   
   def initialize(key, secret, url)
      @client = Coinbase::Wallet::Client.new(api_key: key, api_secret: secret, api_url: url)
+     @file_logger = Logger.new('btc_trader_coinbase_transaction.log', 10, 1024000)
   end
   
   def get_price(currency="USD")
-    @client.buy_price({:currency => currency})
+    @file_logger.info("Price Check Attempt")
+    price = @client.buy_price({:currency => currency})
+    @file_logger.info(price)
+    price
   end
   
   def buy_btc(amt, currency="BTC", p_method=@client.payment_methods.first)
@@ -17,7 +21,8 @@ class CoinbaseTransaction
     buy = @client.primary_account.buy({:amount => amt,
                                        :currency => currency,
                                        :payment_method => p_method.id})
-    #puts buy.status                                           
+    @file_logger.info("BTC Buy Attempt")                                      
+    @file_logger.info(buy)                                           
     case buy.status
     when "completed"
       return true
@@ -31,6 +36,8 @@ class CoinbaseTransaction
     sell = @client.primary_account.sell({:amount => amt,
                        :currency => "BTC",
                        :payment_method => p_method.id})
+    @file_logger.info("BTC Sell Attempt")                        
+    @file_logger.info(sell)  
     case sell.status
     when "completed"
       return true
